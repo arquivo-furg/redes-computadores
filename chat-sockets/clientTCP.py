096f6a54726f4275188d680892fadafdcebacbde
pedromchd <48498905+pedromchd@users.noreply.github.com>
2026-06-10 09:27:09 -0300

    docs: basic inicital documentation for client tcp socket

--- chat-sockets/clientTCP.py
+++ chat-sockets/clientTCP.py
@@ -8,22 +8,26 @@ from constants import *
 
 def main(host: str, port: int):
     with socket(AF_INET, SOCK_STREAM) as client:
+        # Tenta uma conexão ao servidor socket no endereço definido
         try:
             client.connect((host, port))
         except:
             return print(f"Não foi possível conectar-se a {host}:{port}.")
 
+        # Desenha o input da mensagem e o botão de enviar
         field = tk.Entry(window, textvariable=input, width=50)
         field.bind("<Return>", partial(send_message, client))
         field.pack()
+        # O uso de partial é para bindar o socket do cliente com as funções de envio
         send = tk.Button(window, text="Enviar", command=partial(send_message, client))
         send.pack()
-
+        # Evento acionado quando a janela é fechada pela GUI
         window.protocol("WM_DELETE_WINDOW", partial(on_closing, client))
 
+        # Cria e inicia uma nova thread para lidar com o recebimento das mensagens
         threading.Thread(target=recv_messages, args=(client,)).start()
 
-        tk.mainloop()
+        tk.mainloop()  # Inicia a GUI
 
 
 def recv_messages(client: socket):
@@ -74,26 +78,30 @@ def handle_command(command: str, client: socket | None = None, event=None):
         messages.delete(0, tk.END)
 
 
+# Cria uma janela do Tkinter para desenhar a lista de mensagens, input e botão
 window = tk.Tk()
 window.title("Chat")
 
 messages_frame = tk.Frame(window)
 messages_frame.pack()
 
-input = tk.StringVar()
+input = tk.StringVar()  # Variável que guarda o texto da mensagem escrita
 
 scrollbar = tk.Scrollbar(messages_frame)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
+# Lista de mensagens onde serão escritas as mensagens recebidas
 messages = tk.Listbox(messages_frame, height=20, width=75, yscrollcommand=scrollbar.set)
 messages.pack(side=tk.LEFT, fill=tk.BOTH)
 
 
 if __name__ == "__main__":
+    # Admite argumentos passados ao executável através de flags --host e --port
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default=HOST)
     parser.add_argument("-p", "--port", type=int, default=PORT)
 
     args = parser.parse_args()
 
+    # Incilializa o código do cliente repassando o host e port definidos
     main(**vars(args))
